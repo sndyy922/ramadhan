ce3ed9a74dab0e1bd91b370285231ae294bb2645
sndyy922 <fajarkurniasandy9@gmail.com>
2020-04-19 19:06:40 +0700

    19 04 20 19:06 TAMPILAN SISWA PUASA TADARUS

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -10,10 +10,7 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-<<<<<<< HEAD
                 $this->template->load('siswa_kegiatanutama','welcome_message');
-=======
-        $this->template->load('template','welcome_message');
->>>>>>> 2573855998b0bd31da68477409d8ad9f2df6c2d1
+
 	}
 }
